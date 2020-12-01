1b9c940bdd0dbf7bd4fde28e95bfe68b66582931
pmcubom65 <pmcubom@zion.es>
2020-12-01 19:08:47 +0100

    test

--- database/migrations/2020_11_17_183225_create_mensajes_table.php
+++ database/migrations/2020_11_17_183225_create_mensajes_table.php
@@ -26,7 +26,7 @@ class CreateMensajesTable extends Migration
 
             $table->foreign('CHAT_ID')
             ->references('ID')
-            ->on('CHATSCHAT')
+            ->on('chatschat')
             ->onDelete('cascade');
 
             
